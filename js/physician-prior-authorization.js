71c78afb22780ccc14c8deeef47ddeb6de807115
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-01-23 22:51:40 +0400

    Bux Fix

--- js/physician-prior-authorization.js
+++ js/physician-prior-authorization.js
@@ -1,25 +1,21 @@
-window.onload = function(){
-    getInsuranceStatus();
-    getData();
+var querryString = decodeURIComponent(window.location.search);
+querryString = querryString.substring(1);
+var queries = querryString.split("&");
+for(var i = 0; i < queries.length; i++){
+    var incremeter = i++;
+    document.getElementById("patientid").innerHTML = queries[0].replace("param1","");
+    document.getElementById("patientname").innerHTML = queries[1].replace("param2","");
+    document.getElementById("age").innerHTML = queries[2].replace("param3","");
+    document.getElementById("sex").innerHTML = queries[3].replace("param4","");
+    document.getElementById("emiratesid").innerHTML = queries[4].replace("param5","");
+    document.getElementById("insuranceid").innerHTML = queries[5].replace("param6","");
+    document.getElementById("contactnumber").innerHTML = queries[6].replace("param6","");
+    document.getElementById("visittype").innerHTML = "Inpatient"
+    document.getElementById("email").innerHTML = queries[7].replace("param7","");
 }
 
-function getInsuranceStatus(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:3000/api/CoverageDetails", false);
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var obj = JSON.parse(this.responseText);
-            if(obj[0].isCovered === true){
-                document.getElementById("insuranceStatus").innerHTML = "Covered";
-            }else{
-                document.getElementById("insuranceStatus").innerHTML = "Not Covered";
-            }
-            document.getElementById("insuranceResponse").innerHTML = obj[0].insuranceResponseId;
-            document.getElementById("date").innerHTML = obj[0].date;
-                    
-        }
-    };
-    xhttp.send();
+window.onload = function(){
+    getData();
 }
 
 function getData(){
@@ -149,21 +145,7 @@ function autoFile(){
     
 }
 
-var querryString = decodeURIComponent(window.location.search);
-querryString = querryString.substring(1);
-var queries = querryString.split("&");
-for(var i = 0; i < queries.length; i++){
-    var incremeter = i++;
-    document.getElementById("patientid").innerHTML = queries[0].replace("param1","");
-    document.getElementById("patientname").innerHTML = queries[1].replace("param2","");
-    document.getElementById("age").innerHTML = queries[2].replace("param3","");
-    document.getElementById("sex").innerHTML = queries[3].replace("param4","");
-    document.getElementById("emiratesid").innerHTML = queries[4].replace("param5","");
-    document.getElementById("insuranceid").innerHTML = queries[5].replace("param6","");
-    document.getElementById("contactnumber").innerHTML = queries[6].replace("param6","");
-    document.getElementById("visittype").innerHTML = "Inpatient"
-    document.getElementById("email").innerHTML = queries[7].replace("param7","");
-}
+
 
 
 
