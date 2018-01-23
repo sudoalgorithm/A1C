ed51cdcab1a4a0b293a97d97c786d5b08f8c7c34
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-01-23 20:30:06 +0400

    Bug Fix

--- js/home.js
+++ js/home.js
@@ -16,21 +16,39 @@ function onBoardPatientInsuranceCard(){
     var dateOfExpiry = "";
     if(patientid === "123"){
         dateOfExpiry = date[0];
+        document.getElementById("insuranceStatus").innerHTML = "Active";
+        document.getElementById("eligibilityrequestid").innerHTML = "ABCD";
+        document.getElementById("dateofexpiry").innerHTML = date[0];
     }
     if(patientid === "234"){
         dateOfExpiry = date[1];
+        document.getElementById("insuranceStatus").innerHTML = "Active";
+        document.getElementById("eligibilityrequestid").innerHTML = "EFGH";
+        document.getElementById("dateofexpiry").innerHTML = date[1];
     }
     if(patientid === "345"){
         dateOfExpiry = date[2];
+        document.getElementById("insuranceStatus").innerHTML = "Inactive";
+        document.getElementById("eligibilityrequestid").innerHTML = "IJKL"
+        document.getElementById("dateofexpiry").innerHTML = date[2];
     }
     if(patientid === "456"){
         dateOfExpiry = " ";
+        document.getElementById("insuranceStatus").innerHTML = "";
+        document.getElementById("eligibilityrequestid").innerHTML = "";
+        document.getElementById("dateofexpiry").innerHTML = "";
     }
     if(patientid === "678"){
         dateOfExpiry = " ";
+        document.getElementById("insuranceStatus").innerHTML = "";
+        document.getElementById("eligibilityrequestid").innerHTML = "";
+        document.getElementById("dateofexpiry").innerHTML = "";
     }
     if(patientid === "789"){
         dateOfExpiry = " ";
+        document.getElementById("insuranceStatus").innerHTML = "";
+        document.getElementById("eligibilityrequestid").innerHTML = "";
+        document.getElementById("dateofexpiry").innerHTML = "";
     }
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost:3000/api/InsuranceCard",true);
@@ -96,9 +114,7 @@ function getSearch(){
         document.getElementById("email").value = "aishamohammed@test.com"
         document.getElementById("patientnameicf").innerHTML = "Aisha Mohammed";
         document.getElementById("insuranceidicf").innerHTML = "AA-001-900"
-        document.getElementById("insuranceStatus").innerHTML = "Active";
-        document.getElementById("eligibilityrequestid").innerHTML = "ABCD";
-        document.getElementById("dateofexpiry").innerHTML = date[0];
+        
     }
     if(document.getElementById("patientid").value === "234"){
         document.getElementById("paitentname").value = "Aamina Ali";
@@ -110,9 +126,7 @@ function getSearch(){
         document.getElementById("email").value = "aamina.ali@test.com"
         document.getElementById("patientnameicf").innerHTML = "Aamina Ali";
         document.getElementById("insuranceidicf").innerHTML = "BB-007-600"
-        document.getElementById("insuranceStatus").innerHTML = "Active";
-        document.getElementById("eligibilityrequestid").innerHTML = "EFGH";
-        document.getElementById("dateofexpiry").innerHTML = date[1];
+        
     }
     if(document.getElementById("patientid").value === "345"){
         document.getElementById("paitentname").value = "AbdulRazzak Ahmed";
@@ -124,9 +138,7 @@ function getSearch(){
         document.getElementById("email").value = "aahmed@test.com"
         document.getElementById("patientnameicf").innerHTML = "AbdulRazzak Ahmed";
         document.getElementById("insuranceidicf").innerHTML = "CC-002-400"
-        document.getElementById("insuranceStatus").innerHTML = "Inactive";
-        document.getElementById("eligibilityrequestid").innerHTML = "IJKL"
-        document.getElementById("dateofexpiry").innerHTML = date[2];
+        
     }
     if(document.getElementById("patientid").value === "456"){
         document.getElementById("paitentname").value = "Hanifa Shaheen";
@@ -134,13 +146,13 @@ function getSearch(){
         document.getElementById("sex").value = "Female";
         document.getElementById("emiratesid").value = "764-1974-123456-2";
         document.getElementById("insuranceid").value = "AA-001-950"
-        document.getElementById("contactnumber").value = "971765432632";
+        document.getElementById("contactnumber").value = document.getElementById("insuranceStatus").innerHTML = "";
+        document.getElementById("eligibilityrequestid").innerHTML = "";
+        document.getElementById("dateofexpiry").innerHTML = "";"971765432632";
         document.getElementById("email").value = "hanifa.shaheen@test.com"
         document.getElementById("patientnameicf").innerHTML = "Hanifa Shaheen";
         document.getElementById("insuranceidicf").innerHTML = "AA-001-950"
-        document.getElementById("insuranceStatus").innerHTML = "";
-        document.getElementById("eligibilityrequestid").innerHTML = "";
-        document.getElementById("dateofexpiry").innerHTML = "";
+        
     }
     if(document.getElementById("patientid").value === "678"){
         document.getElementById("paitentname").value = "Hakeem Hassan";
@@ -152,12 +164,8 @@ function getSearch(){
         document.getElementById("email").value = "hakeem.hassan@test.com"
         document.getElementById("patientnameicf").innerHTML = "Hakeem Hassan";
         document.getElementById("insuranceidicf").innerHTML = "BB-007-650"
-        document.getElementById("insuranceStatus").innerHTML = "Active";
-        document.getElementById("eligibilityrequestid").innerHTML = "ABCD";
-        document.getElementById("dateofexpiry").innerHTML = "July, 2018";
-        document.getElementById("insuranceStatus").innerHTML = "";
-        document.getElementById("eligibilityrequestid").innerHTML = "";
-        document.getElementById("dateofexpiry").innerHTML = "";
+        
+       
     }
     if(document.getElementById("patientid").value === "789"){
         document.getElementById("paitentname").value = "Hesham Osama";
@@ -169,9 +177,7 @@ function getSearch(){
         document.getElementById("email").value = "hesham.osama@test.com"
         document.getElementById("patientnameicf").innerHTML = "Hesham Osama";
         document.getElementById("insuranceidicf").innerHTML = "CC-002-450"
-        document.getElementById("insuranceStatus").innerHTML = "";
-        document.getElementById("eligibilityrequestid").innerHTML = "";
-        document.getElementById("dateofexpiry").innerHTML = "";
+        
     }
 }
 
