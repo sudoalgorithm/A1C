d13d10007e5f5e0d28792e8500583b0ede0cc928
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-01-23 19:09:24 +0400

    Bug Fix

--- js/insurance-company.js
+++ js/insurance-company.js
@@ -27,7 +27,7 @@ function getData(){
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
             document.getElementById("name").innerHTML = obj.patientFirstName;
-            document.getElementById("insuranceid").textContent = obj.patientInsuranceId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
         }
     };
     xhttp.send();
