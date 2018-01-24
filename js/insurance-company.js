d6bfd78ef07b1c536f40002189b68d1a3c56d2b5
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-01-24 15:00:02 +0400

    Bug Fix

--- js/insurance-company.js
+++ js/insurance-company.js
@@ -4,7 +4,7 @@ var queries = querryString.split("&");
 
 function getData(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:3000/api/InsuranceCard/"+localStorage.getItem("patientid"),false);
+    xhttp.open("GET", "http://localhost:3000/api/InsuranceCard/123",false);
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
@@ -25,7 +25,7 @@ function getData(){
 
 function getPatientData(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/"+localStorage.getItem("patientid"),false);
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/123",false);
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
