1656a21d7cd2a031622f332d03208c4de48fda02
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-01-24 20:12:38 +0400

    Bug Fix

--- js/patient.js
+++ js/patient.js
@@ -1,10 +1,57 @@
-var querryString = decodeURIComponent(window.location.search);
-querryString = querryString.substring(1);
-var queries = querryString.split("&");
-
+document.body.onload = function(){
+    getPatientData();
+    getPatientData1();
+    getPatientData2();
+    getPatientData3();
+    getPatientData4();
+    getPatientData5();
+    getPatientData6();
+}
 function getPatientData(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/"+queries[0].replace("param1",""),false);
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/123",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+        }
+    };
+    xhttp.send();
+}
+
+function getPatientData1(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/234",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+      
+            
+        }
+    };
+    xhttp.send();
+}
+
+function getPatientData2(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/345",false);
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
@@ -23,28 +70,88 @@ function getPatientData(){
     xhttp.send();
 }
 
-function sendDataPA(){
-    var patientid = document.getElementById("patientid").value;
-    queryBuilder = "?param1" + patientid; 
-    window.location.href = "physician-prior-authorization.html" + queryBuilder;
+function getPatientData3(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/456",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+            
+        }
+    };
+    xhttp.send();
 }
 
-function sendDataIC(){
-    var patientid = document.getElementById("patientid").value;
-    queryBuilder = "?param1" + patientid;
-    window.location.href = "insurance-company.html" + queryBuilder;
+
+function getPatientData4(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/567",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+ 
+            
+        }
+    };
+    xhttp.send();
 }
 
-function sendDataHCT(){
-    
-    var patientid = document.getElementById("patientid").value;
-    queryBuilder = "?param1" + patientid;
-    window.location.href = "hospital-claims-teams.html" + queryBuilder;
+function getPatientData5(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/678",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+ 
+            
+        }
+    };
+    xhttp.send();
 }
-
-function sendDataHome(){
-    var patientid = document.getElementById("patientid").value;
-    queryBuilder = "?param1" + patientid;
-    window.location.href = "home.html" + queryBuilder;
+function getPatientData6(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/api/PatientRecord/789",false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            document.getElementById("patientid").innerHTML = obj.patientId;
+            document.getElementById("patientname").innerHTML = obj.patientFirstName;
+            document.getElementById("age").innerHTML = obj.patientAge;
+            document.getElementById("sex").innerHTML = obj.patientGender;
+            document.getElementById("emiratesid").innerHTML = obj.patientEmiratesId;
+            document.getElementById("insuranceid").innerHTML = obj.patientInsuranceId;
+            document.getElementById("contactnumber").innerHTML = obj.patientContactNumber;
+            document.getElementById("visittype").innerHTML = obj.patientVisitType;
+            document.getElementById("email").innerHTML = obj.patientEmailId;
+            
+        }
+    };
+    xhttp.send();
 }
-
